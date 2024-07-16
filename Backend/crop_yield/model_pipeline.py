f66971611a76897b71a69d774a1f4a70f011014f
siam3h <philosiama@gmail.com>
2024-07-16 13:49:51 +0300

    Keyerror in the crop yield endpoint solved

--- Backend/crop_yield/model_pipeline.py
+++ Backend/crop_yield/model_pipeline.py
@@ -1,4 +1,3 @@
-# predictor/model_pipeline.py
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
@@ -14,24 +13,24 @@ class CropYieldModel:
         self.train_model()
         
     def prepare_data(self):
-        df_yield = pd.read_csv('yield.csv')
+        df_yield = pd.read_csv(r'crop_yield/data/yield.csv')
         df_yield.rename(columns={"Value": "hg/ha_yield"}, inplace=True)
-        df_yield = df_yield.drop(['Year Code','Element Code', 'Element','Year Code','Area Code','Domain Code', 'Domain','Unit','Item Code'], axis=1)
+        df_yield = df_yield.drop(['Year Code', 'Element','Year Code','Area Code','Domain Code', 'Domain','Unit','Item Code'], axis=1)
         df_yield = df_yield.dropna()
         
-        df_rain = pd.read_csv('rainfall.csv')
+        df_rain = pd.read_csv(r'crop_yield/data/rainfall.csv')
         df_rain['average_rain_fall_mm_per_year'] = pd.to_numeric(df_rain['average_rain_fall_mm_per_year'], errors='coerce')
         df_rain = df_rain.dropna()
         
         df_main = pd.merge(df_yield, df_rain, on=['Year','Area'], how='outer')
         
-        dataframe_pesticide = pd.read_csv('pesticides.csv')
+        dataframe_pesticide = pd.read_csv(r'crop_yield/data/pesticides.csv')
         dataframe_pesticide.rename(columns={"Value": "pesticides_tonnes"}, inplace=True)
         dataframe_pesticide = dataframe_pesticide.drop(['Element','Domain','Unit','Item'], axis=1)
         
         df_main = pd.merge(df_main, dataframe_pesticide, on=['Year','Area'])
         
-        dataframe_temp = pd.read_csv('temp.csv')
+        dataframe_temp = pd.read_csv(r'crop_yield/data/temp.csv')
         dataframe_temp.rename(columns={"year": "Year", "country": 'Area'}, inplace=True)
         
         self.df_main = pd.merge(df_main, dataframe_temp, on=['Year','Area'])
@@ -42,7 +41,7 @@ class CropYieldModel:
         X = df_main.drop('hg/ha_yield', axis=1)
         y = df_main['hg/ha_yield']
         
-        numeric_features = ['average_rain_fall_mm_per_year', 'pesticides_tonnes', 'temperature']
+        numeric_features = ['average_rain_fall_mm_per_year', 'pesticides_tonnes', 'avg_temp']
         categorical_features = ['Year', 'Area']
 
         numeric_transformer = Pipeline(steps=[
